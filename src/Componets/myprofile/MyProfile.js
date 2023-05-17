e3a937baeca971096a57dc55e239298d02ee6143
Nendy Azhad Nurahman <azhadnurahmann@outlook.com>
2023-05-17 15:43:01 +0700

    ganti foto

--- src/Componets/myprofile/MyProfile.js
+++ src/Componets/myprofile/MyProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Profile.css";
 // import { Link } from "react-router-dom";
-const fp = require("../../img/fp.jpg");
+const fp = require("../../img/fp2.jpg");
 const resume = require("../../Documents/nendy-portfolio.pdf");
 
 function MyProfile() {
